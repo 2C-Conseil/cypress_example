019590ee00e872ccb317699913ce8a0d13bf0fff
phbord <phbord@2cconseil.com>
2023-09-13 17:39:03 +0200

    sc 3

--- cypress/e2e/spec.scenario3.cy.js
+++ cypress/e2e/spec.scenario3.cy.js
@@ -3,8 +3,8 @@ const { createCanvas, loadImage } = require('canvas');
 import '../support/commands';
 
 
-describe('Scenario 01 - Filtrage des tests du dossier "CNAV"', () => {
-  it.only("Itération 1", () => {
+describe('Scenario 01 - Comparaison de données provenant d\'une requête et d\'un mock', () => {
+  it("Itération 1", () => {
     // l'interception de la requête ne peut se faire qu'avant l'action (bouton ou changement d'url) déclenchant la requête
     cy.intercept('GET', '**/api/comments?content=87', req => {
       req.reply({
@@ -12,40 +12,42 @@ describe('Scenario 01 - Filtrage des tests du dossier "CNAV"', () => {
         fixture: 'comments.json'
       });
     }).as('reqReply');
-    cy.visit('https://grafikart.fr/tutoriels/carrousel-javascript-87');
-    cy.wait(1000);
-    cy.get('.comment-list .comment')
-      .first().children('.comment__content')
-      .scrollIntoView()
-      .as('comment');
+
+    // Action
+    cy.visit('https://grafikart.fr/tutoriels/carrousel-javascript-87')
+      .wait(1000);
 
     // Détecte et s'arrête sur la 1ère requête
     cy.wait('@reqReply').then(res => {
       cy.fixture("comments").then(fixture => {
-        console.log(fixture,'>>>>>>>>> res:', [res.response.body[0]]);
         expect([res.response.body[0]]).to.deep.eq(fixture);
       });
     });
-
-
-
-    /* cy.intercept('GET', '/api/comments?content=87', { fixture: 'comments.json' }, (req) => {
-      console.log('>>>>>>>>> req:', req);
-      cy.wait(500);
-      
-        //expect([req.body[0]]).to.deep.eq(fixture);
-    }); */
-    /* cy.fixture('comments.json').then(fixture => {
-      console.log('>>>>>>>>> fixture:', fixture);
-      cy.wait(500);
-      cy.intercept('GET', '/api/comments?content=87', req => {
-        console.log('>>>>>>>>> req:', req);
-        req.continue(res => {
-          expect([res.body[0]]).to.deep.eq(fixture);
-          console.log('>>>>>>>>> res:', [res.body[0]]);
-        });
+  })
+  
+  it.only("Itération 2", () => {
+    // l'interception de la requête ne peut se faire qu'avant l'action (bouton ou changement d'url) déclenchant la requête
+    cy.intercept('GET', '**/api/courses/', req => {
+      req.reply({
+        statusCode: 200,
+        fixture: 'courses.json'
       });
-    }); */
-    //cy.get('.comment__author').contains("Thibert Nkubonage")
-  });
+    }).as('reqReply');
+
+    // Action
+    cy.visit('https://dyma.fr/')
+      .wait(1000);
+
+    // Détecte et s'arrête sur la 1ère requête
+    cy.wait('@reqReply').then(res => {
+      cy.fixture("courses").then(fixture => {
+        expect([res.response.body[0]]).to.deep.eq(fixture);
+
+        cy.get('.popular-tech-container > a').first().find('h3')
+          .scrollIntoView()
+          .contains(res.response.body[0].title);
+        expect(res.response.body[0].title).to.deep.eq(fixture[0].title);
+      });
+    });
+  })
 });
